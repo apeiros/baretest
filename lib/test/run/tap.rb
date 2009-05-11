3dc36d25418ae72d44b961b312cb971101664fb3
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-05-11 12:58:09 +0200

    Added TAP (Test Anything Protocol) output formatter.

---
+++ lib/test/run/tap.rb
@@ -0,0 +1,24 @@
+module Test
+	class Run
+		module TAP
+			def run_all(suite)
+				puts "TAP version 13"
+				count = proc { |acc,csuite| acc+csuite.tests.size+csuite.suites.inject(0, &count) }
+				puts "1..#{count[0, suite]}"
+				@current = 0
+				super
+			end
+
+			def run_test(assertion)
+				rv = super
+				printf "%sok %d - %s%s\n",
+					rv.status == :success ? '' : 'not ',
+					@current+=1,
+					rv.message,
+					rv.status == :success ? '' : " # #{rv.status}"
+			end
+		end
+	end
+
+	@extender["test/run/tap"] = Run::TAP
+end
