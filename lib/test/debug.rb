1d501411a274f04c5fc14b7fffcf39ec4993d7fd
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-05-24 22:04:58 +0200

    Improved debug methods a bit.

--- lib/test/debug.rb
+++ lib/test/debug.rb
@@ -1,13 +1,13 @@
 module Test
 	class Suite
 		def inspect
-			sprintf "#<%s:%08x %s>", self.class, object_id>>1, @name
+			sprintf "#<%s:%08x %p>", self.class, object_id>>1, @name
 		end
 	end
 
 	class Assertion
 		def inspect
-			sprintf "#<%s:%08x %s>", self.class, object_id>>1, @message
+			sprintf "#<%s:%08x @suite=%p %p>", self.class, object_id>>1, @suite, @message
 		end
 	end
 end
