2b14dd1ec6e0febcd5d333ab0fa2b417d4cf2693
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-05-31 20:37:39 +0200

    Updated. Added Suite#to_s and Assertion#to_s.

--- lib/test/debug.rb
+++ lib/test/debug.rb
@@ -8,12 +8,20 @@
 
 module Test
 	class Suite
+		def to_s
+			sprintf "%s %s", self.class, @name
+		end
+
 		def inspect
 			sprintf "#<%s:%08x %p>", self.class, object_id>>1, @name
 		end
 	end
 
 	class Assertion
+		def to_s
+			sprintf "%s %s", self.class, @message
+		end
+
 		def inspect
 			sprintf "#<%s:%08x @suite=%p %p>", self.class, object_id>>1, @suite, @message
 		end
