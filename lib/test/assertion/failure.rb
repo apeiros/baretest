840dc569dff5125927e0aac883033a0603252d81
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-06-05 18:39:44 +0200

    Added the exception assertions may raise to provide a failure reason, and still be considered a failure instead of an error.

---
+++ lib/test/assertion/failure.rb
@@ -0,0 +1,14 @@
+#--
+# Copyright 2009 by Stefan Rusterholz.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#++
+
+
+
+module Test
+	class Assertion
+	  class Failure < StandardError
+		end
+	end
+end
