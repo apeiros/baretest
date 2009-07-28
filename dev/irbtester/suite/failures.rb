6ee9e7c8419f28fe90527a86a43cc931397cfc5b
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-07-28 11:18:04 +0200

    Added a tiny example testsuite to experiment with irb-mode.

---
+++ dev/irbtester/suite/failures.rb
@@ -0,0 +1,26 @@
+#--
+# Copyright 2009 by Stefan Rusterholz.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#++
+
+
+
+BareTest.define "Failure" do
+  setup do
+    @a = 1
+    @b = 2
+  end
+
+        assert "This one should fail and thus drop you into irb" do
+          c = 3
+          d = 4
+          @a == d
+        end
+      
+        assert "This one should error and thus drop you into irb" do
+          c = 3
+          d = 4
+          raise "error!"
+        end
+end
