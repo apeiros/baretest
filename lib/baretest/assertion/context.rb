d842badd0a96c54b7f080470d6cb3c0c6db27aa2
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-11-23 15:45:10 +0100

    Added Assertion::Context, which is the designated clean object to execute the assertion in.

---
+++ lib/baretest/assertion/context.rb
@@ -0,0 +1,14 @@
+#--
+# Copyright 2009 by Stefan Rusterholz.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#++
+
+
+
+module BareTest
+  class Assertion
+    class Context
+    end
+  end
+end
