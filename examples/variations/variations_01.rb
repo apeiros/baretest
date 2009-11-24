2d41f161618b3364f736f3a7f321f1d34dc2ba2f
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2009-11-24 19:54:41 +0100

    Added an example of setup variations.

---
+++ examples/variations/variations_01.rb
@@ -0,0 +1,25 @@
+require 'pp'
+
+BareTest.suite "Variations 01" do
+  setup do
+    puts "n1"
+  end
+  setup do
+    puts "n2"
+  end
+  setup :a, "a1" do
+    puts "a1"
+  end
+  setup :a, "a2" do
+    puts "a2"
+  end
+  setup :b, "b1" do
+    puts "b1"
+  end
+  setup :b, "b2" do
+    puts "b2"
+  end
+  assert "variants, a: :a, b: :b" do
+    true
+  end
+end
