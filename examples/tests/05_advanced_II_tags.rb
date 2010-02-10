784f9a6676ac4d4dcd260c331dc2729e95d83f91
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2010-02-10 15:23:03 +0100

    Updated examples.

---
+++ examples/tests/05_advanced_II_tags.rb
@@ -0,0 +1,12 @@
+BareTest.suite do
+  suite "focus", :tags => :focus do
+    assert "a" do
+      true
+    end
+  end
+  suite "hocus", :tags => :hocus do
+    assert "b" do
+      true
+    end
+  end
+end
