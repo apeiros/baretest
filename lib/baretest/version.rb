469cb644ae840a6220dfb0c5875c87d9173bf58c
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2010-02-10 22:13:53 +0100

    Updated VERSION.

--- lib/baretest/version.rb
+++ lib/baretest/version.rb
@@ -12,17 +12,21 @@ module BareTest
   module VERSION
 
     # The major version number
-    MAJOR = 0
+    MAJOR      = 0
 
     # The minor version number
-    MINOR = 3
+    MINOR      = 4
 
     # The tiny version number
-    TINY  = 0
+    TINY       = 0
 
+    # Prerelease number - nil for release versions
+    PRERELEASE = 1
+
+    STRING     = %{#{MAJOR}.#{MINOR||0}.#{TINY||0}#{".pre#{PRERELEASE}" if PRERELEASE}}
     # The version as a string
     def self.to_s
-      "#{MAJOR}.#{MINOR||0}.#{TINY||0}"
+      STRING
     end
   end
 end
