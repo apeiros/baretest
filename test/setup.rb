7d6529b2f73ee1606dac87393284fccc368800ed
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2010-02-17 19:42:49 +0100

    Added some comments to the setup file.

--- test/setup.rb
+++ test/setup.rb
@@ -1,8 +1,12 @@
+# Add PROJECT/lib to $LOAD_PATH
 $LOAD_PATH.unshift(File.expand_path("#{__FILE__}/../../lib"))
+
+# Ensure baretest is required
 require 'baretest'
 
+# Some defaults on BareTest (see Kernel#BareTest)
 BareTest do
-  require_baretest "0.4"
-  require_ruby     "1.8.6"
-  use              :support
+  require_baretest "0.4"    # minimum baretest version to run these tests
+  require_ruby     "1.8.6"  # minimum ruby version to run these tests
+  use              :support # Use :support in all suites
 end
