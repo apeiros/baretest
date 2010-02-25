dea3d59ad25eac9768b295caeb626069711e7814
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2010-02-25 23:46:28 +0100

    Added handling of spying methods, which raise a RR::Errors::RRError upon dissatisfaction.

--- lib/baretest/use/rr.rb
+++ lib/baretest/use/rr.rb
@@ -11,6 +11,10 @@ BareTest.new_component :rr do
 
   BareTest::Assertion::Context.send :include, RR::Adapters::RRMethods
 
+  handle_verification_exceptions RR::Errors::RRError do |assertion, phase, exception|
+    ::BareTest::Status.new(assertion, :failure, :verify, nil, exception.message)
+  end
+
   teardown do
     begin
       ::RR.verify
